9e90c67fc4f7af8c31b88ac8772321ecdb44ade4
ChangHyeonPark <ckdgusqkr0803@gmail.com>
2021-11-09 06:16:50 +0900

    앱 처음 실행 시 GPS 적용 안되던 오류 해결
    위도 경도 Test 버튼 추가
    test 버튼과 출발지 선택 클릭 시 퍼미션 검사.
    권한 없음 시 실행되지 않고 앱 재실행(거절) 또는 앱 재설치(다시보지 않기)
    Toast 띄워줌

--- app/src/main/java/com/opensource/seebus/MainActivity.java
+++ app/src/main/java/com/opensource/seebus/MainActivity.java
@@ -1,5 +1,6 @@
 package com.opensource.seebus;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -8,61 +9,119 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.opensource.seebus.startingPoint.StartingPointActivity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener, View.OnClickListener {
 
     private TextView textViewGPS;
     private double longitude;
     private double latitude;
+    Location location;
+
+    //퍼미션 여부
+    boolean locationFindPermission;
+    boolean locationCoursePermission;
+
+    Button locationTest;
+    Button startingPointButton;
+
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button startingPointButton=findViewById(R.id.startingPointButton);
-        startingPointButton.setOnClickListener(view -> {
-            Intent startingPointIntent= new Intent(this, StartingPointActivity.class);
-            startingPointIntent.putExtra("longitude",longitude);
-            startingPointIntent.putExtra("latitude",latitude);
-            startActivity(startingPointIntent);
-        });
+        startingPointButton = findViewById(R.id.startingPointButton);
+        locationTest = findViewById(R.id.locationTest);
 
+        startingPointButton.setOnClickListener(this);
+        locationTest.setOnClickListener(this);
 
-        //TODO GPS최초실행시 동의 누르고 반영이 바로 안되는 현상
-        textViewGPS=findViewById(R.id.textViewGPS);
-        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if ( Build.VERSION.SDK_INT >= 23 &&
-                ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
-            ActivityCompat.requestPermissions( this, new String[] {  android.Manifest.permission.ACCESS_FINE_LOCATION  },
-                    0 );
+        textViewGPS = findViewById(R.id.textViewGPS);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationFindPermission = true;
         }
-        else{
-            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            longitude = location.getLongitude();
-            latitude = location.getLatitude();
 
-            textViewGPS.setText(
-                    "위도 : " + longitude + "\n" +
-                            "경도 : " + latitude + "\n"
-            );
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationCoursePermission = true;
+        }
 
-            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                    1000,
-                    1,
-                    gpsLocationListener);
-            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                    1000,
-                    1,
-                    gpsLocationListener);
+        if (!locationFindPermission || !locationCoursePermission) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION},
+                    200);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (locationFindPermission == true && locationCoursePermission == true) {
+            final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+            if (location == null) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                    return;
+                }
+
+                lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 1, this);
+                location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            }
+
+            if (location != null) {
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+            }
+
+            if (v == locationTest) {
+                textViewGPS.setText(
+                        "위도 : " + longitude + "\n" +
+                                "경도 : " + latitude + "\n"
+                );
+
+                lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                        1000,
+                        1,
+                        gpsLocationListener);
+                lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                        1000,
+                        1,
+                        gpsLocationListener);
+
+            } else if (v==startingPointButton) {
+                Intent startingPointIntent = new Intent(this, StartingPointActivity.class);
+                startingPointIntent.putExtra("longitude", longitude);
+                startingPointIntent.putExtra("latitude", latitude);
+                startActivity(startingPointIntent);
+            }
+        } else {
+            showToast("위치 권한 설정이 필요합니다.\n어플리케이션을 재실행하거나 재설치해주세요.");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int [] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 200 && grantResults.length >0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                locationFindPermission = true;
+            if (grantResults[1] == PackageManager.PERMISSION_GRANTED)
+                locationCoursePermission = true;
         }
     }
 
@@ -78,4 +137,15 @@ public class MainActivity extends AppCompatActivity {
             );
         }
     };
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+    }
+
+    private void showToast(String string) {
+        Toast toast = Toast.makeText(this,string,Toast.LENGTH_SHORT);
+        toast.show();
+    }
 }
+
