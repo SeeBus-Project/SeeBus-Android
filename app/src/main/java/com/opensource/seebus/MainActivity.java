f7171088d7c2adcd7c1769c9abf17b96f4e603db
ChangHyeonPark <ckdgusqkr0803@gmail.com>
2021-11-09 15:18:22 +0900

    GPS 수정
    + 컴파일 에러 없애기 위해 형식적  퍼미션 체크 문장 추가

--- app/src/main/java/com/opensource/seebus/MainActivity.java
+++ app/src/main/java/com/opensource/seebus/MainActivity.java
@@ -22,22 +22,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-public class MainActivity extends AppCompatActivity implements LocationListener, View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private TextView textViewGPS;
     private double longitude;
     private double latitude;
-    Location location;
+    LocationManager lm;
 
-    //퍼미션 여부
-    boolean locationFindPermission;
-    boolean locationCoursePermission;
-
-    Button locationTest;
     Button startingPointButton;
 
-    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
-    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
+    private boolean onlySelfTestValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,103 +39,92 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         setContentView(R.layout.activity_main);
 
         startingPointButton = findViewById(R.id.startingPointButton);
-        locationTest = findViewById(R.id.locationTest);
-
-        startingPointButton.setOnClickListener(this);
-        locationTest.setOnClickListener(this);
-
         textViewGPS = findViewById(R.id.textViewGPS);
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            locationFindPermission = true;
-        }
+        onlySelfTestValue = onlySelfTest();
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            locationCoursePermission = true;
-        }
-
-        if (!locationFindPermission || !locationCoursePermission) {
+        if (onlySelfTestValue == false) {
             ActivityCompat.requestPermissions(this, new String[]{
                             Manifest.permission.ACCESS_FINE_LOCATION,
                             Manifest.permission.ACCESS_COARSE_LOCATION},
                     200);
+        } else {
+            //최초실행이 아니고 퍼미션이 트루라면 앱을 켜자마자 현재위치 찾기
+            whenSelfTestValueIsTrue();
+        }
+
+        startingPointButton.setOnClickListener(view -> {
+            if (onlySelfTest() == false) {
+                showToast("위치 권한 설정이 필요합니다.\n어플리케이션을 재실행하거나 재설치해주세요.");
+            } else if (longitude != 0.0 && latitude != 0.0) {
+                // 최초실행이 아닐때 이미 위에서 현재위치를 가져왔으므로 반복해서 위치를 얻지 않는다.
+                goStartingPointActivity();
+            } else {
+                // 최초실행하고 퍼미션이 트루라면 onCreate()의 if else 문이 작동하지 않으므로 해당 함수를 호출해준다.
+                // 물론 캐시에 저장된 위치를 사용해서 정확도가 떨어지지만 다시 홈화면으로 돌아올 경우 위치는 갱신되어있으므로
+                // 조금? 좋아진거같다.
+                whenSelfTestValueIsTrue();
+                goStartingPointActivity();
+            }
+        });
+    }
+
+    private void goStartingPointActivity() {
+        Intent startingPointIntent = new Intent(this, StartingPointActivity.class);
+        startingPointIntent.putExtra("longitude", longitude);
+        startingPointIntent.putExtra("latitude", latitude);
+        startActivity(startingPointIntent);
+    }
+
+    private boolean onlySelfTest() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
         }
     }
 
-    @Override
-    public void onClick(View v) {
-        if (locationFindPermission == true && locationCoursePermission == true) {
-            final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    private void whenSelfTestValueIsTrue() {
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-            if (location == null) {
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) { return; }
+        // 퍼미션 체크용 (컴파일 하려면 있어야 함.. 의미는 없음)
 
-                    return;
-                }
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        longitude = location.getLongitude();
+        latitude = location.getLatitude();
+        textViewGPS.setText(
+                "위도 : " + longitude + "\n" +
+                        "경도 : " + latitude + "\n"
+        );
 
-                lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 1, this);
-                location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            }
-
-            if (location != null) {
-                longitude = location.getLongitude();
-                latitude = location.getLatitude();
-            }
-
-            if (v == locationTest) {
-                textViewGPS.setText(
-                        "위도 : " + longitude + "\n" +
-                                "경도 : " + latitude + "\n"
-                );
-
-                lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                        1000,
-                        1,
-                        gpsLocationListener);
-                lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                        1000,
-                        1,
-                        gpsLocationListener);
-
-            } else if (v==startingPointButton) {
-                Intent startingPointIntent = new Intent(this, StartingPointActivity.class);
-                startingPointIntent.putExtra("longitude", longitude);
-                startingPointIntent.putExtra("latitude", latitude);
-                startActivity(startingPointIntent);
-            }
-        } else {
-            showToast("위치 권한 설정이 필요합니다.\n어플리케이션을 재실행하거나 재설치해주세요.");
-        }
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                0,
+                0,
+                gpsLocationListener);
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                0,
+                0,
+                gpsLocationListener);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int [] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == 200 && grantResults.length >0) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                locationFindPermission = true;
-            if (grantResults[1] == PackageManager.PERMISSION_GRANTED)
-                locationCoursePermission = true;
-        }
     }
 
     final LocationListener gpsLocationListener = new LocationListener() {
+        @Override
         public void onLocationChanged(Location location) {
-
-            double longitude = location.getLongitude();
-            double latitude = location.getLatitude();
-
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
             textViewGPS.setText(
                     "위도 : " + longitude + "\n" +
                             "경도 : " + latitude + "\n"
             );
         }
     };
-
-    @Override
-    public void onLocationChanged(@NonNull Location location) {
-
-    }
 
     private void showToast(String string) {
         Toast toast = Toast.makeText(this,string,Toast.LENGTH_SHORT);
